89f79571111ef7e707fa47772c1e4c40112f9e03
Thanh-Phong-1 <tranthanhphong551@gmail.com>
2023-01-26 20:48:42 +0700

    workshop

---
+++ Exercise2/src/java/controller/CalServlet.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+public class CalServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet CalServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet CalServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       Double num1= Double.parseDouble(request.getParameter("num1"));
+       Double num2=  Double.parseDouble(request.getParameter("num2"));
+       String op =request.getParameter("op");
+       PrintWriter out= response.getWriter();
+       Object  result=0;
+       switch(op){
+           case "add":
+               result=num1+num2;
+               break;
+            case "sub":
+               result=num1-num2;
+               break;
+            case "mul":
+               result=num1*num2;
+               break;
+            case "div":
+             if(num2 == 0){
+              result = "Chia cho 0";
+             }
+            else{result=num1/num2;}
+            break;
+        }
+       ///luu num 1 num 2 bao toan trang thai cua form
+         request.setAttribute("num1", num1);
+         request.setAttribute("num2", num2);
+    
+       request.setAttribute("result", result);//lưu result vao request
+       request.getRequestDispatcher("/calculator.jsp").forward(request,response);//forward den trang calculator.jsp"
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
